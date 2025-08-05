2c8b722742ffccecbd23b8c2b13265742ad88966
George Oluoko <otienog1@gmail.com>
2025-08-05 17:00:12 +0300

    tinkering

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className="scroll-smooth">
-            <body className={`${inter.className} pt-8`}>
+            <body className={`${inter.className} md:pt-8`}>
                 <Navigation />
                 <main>{children}</main>
                 <Footer />
